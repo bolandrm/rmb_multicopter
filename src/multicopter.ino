e633991c9ad2acab821b3db333b9961d36c0b9f4
Ryan Boland <bolandryanm@gmail.com>
2015-02-10 18:29:17 -0500

    starting to move to teensy

--- src/multicopter.ino
+++ src/multicopter.ino
@@ -1,22 +1,25 @@
 #define SERIAL_PORT_SPEED 115200
 
-#include "imu.h"
-#include "debugger.h"
-#include "flight_controller.h"
-#include "serial_commands.h"
-#include "remote_control.h"
+// #include "imu.h"
+// #include "debugger.h"
+// #include "flight_controller.h"
+// #include "serial_commands.h"
+// #include "remote_control.h"
 
 void setup() {
   Serial.begin(SERIAL_PORT_SPEED);
-  imu_init();
-  fc_init();
-  rc_init();
+//  imu_init();
+//  fc_init();
+//  rc_init();
 }
 
 void loop() {
-  while(!imu_read());
-  serial_commands_process();
-  rc_read_values();
-  fc_process();
-  debugger_print();
+//  while(!imu_read());
+//  serial_commands_process();
+//  rc_read_values();
+//  fc_process();
+//  debugger_print();
+  Serial.println("ok");
+  Serial.println(F_CPU);
+  delay(1000);
 }
