0bce8150cf1eaca45d3c371d520d78151c71b4c9
Ryan Boland <bolandryanm@gmail.com>
2016-01-21 18:05:10 -0500

    relocate ui

--- ui/main.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    bounds: {
-      width: 1500,
-      height: 800
-    },
-    resizable: false
-  });
-});
