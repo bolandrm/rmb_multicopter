85e9eb663107754113772a13b3c0d49fc8ab95f9
Ryan Boland <bolandryanm@gmail.com>
2016-02-21 02:36:43 -0500

    add tabs

--- js_ui2/app/components/toolbar.jsx
+++ js_ui2/app/components/toolbar.jsx
@@ -10,15 +10,23 @@ class Toolbar extends React.Component {
 
     return (
       <div className='well toolbar'>
-        <div className='form-inline'>
-          <select className='form-control' disabled={this.props.connected}
+        <ul className="nav nav-tabs">
+          <li className="active"><a href="#">Graphs</a></li>
+          <li><a href="#">PIDs</a></li>
+          <li><a href="#">Remote Control</a></li>
+          <li><a href="#">IMU</a></li>
+          <li><a href="#">Scheduler</a></li>
+        </ul>
+
+        <div className='connection form-inline'>
+          <select className='form-control' disabled={this.props.connected || this.props.busy}
             onChange={this.deviceChanged} value={this.props.selectedDevice}>
             {this.props.devices.map((device, i) => {
               return <option key={i}>{device}</option>
             })}
           </select>
 
-          {!this.props.connected &&
+          {!this.props.connected && !this.props.busy &&
             <button className='btn btn-default' onClick={actions.refreshDevices}>Refresh</button>
           }
 
