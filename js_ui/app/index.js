5d2a2a3ce615c61b715c22dcae71f09f91de05e1
Ryan Boland <bolandryanm@gmail.com>
2016-03-06 19:09:58 -0500

    remove reducers; redux deps

--- js_ui/app/index.js
+++ js_ui/app/index.js
@@ -1,7 +1,6 @@
 import 'babel-polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
 import App from './components/app'
 
 import './styles/app.scss'
