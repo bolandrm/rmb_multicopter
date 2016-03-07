58f7b7c76314acd8675d45248116f9dc6f61b4da
Ryan Boland <bolandryanm@gmail.com>
2016-03-06 19:04:57 -0500

    remove chrome ui

--- js_ui/main.js
+++ js_ui/main.js
@@ -1,19 +1,103 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  var windowWidth = 1500;
-  var windowHeight = 800;
+'use strict';
 
-  chrome.app.window.create('index.html', {
-    outerBounds: {
-      width: windowWidth,
-      height: windowHeight,
-      left: screen.availWidth - windowWidth,
-      top: screen.availHeight - windowHeight
-    },
-    resizable: false
-  });
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+// const crashReporter = electron.crashReporter;
+const shell = electron.shell;
+let menu;
+let template;
+let mainWindow = null;
+
+// crashReporter.start();
+
+if (process.env.NODE_ENV === 'development') {
+  require('electron-debug')();
+}
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
 
-// chrome.commands.onCommand.addListener(function(command) {
-//   console.log('Command:', command);
-// });
-// 
+app.on('ready', () => {
+  var windowWidth = 1500;
+  var windowHeight = 900;
+
+  mainWindow = new BrowserWindow({
+    width: windowWidth,
+    height: windowHeight,
+    resizable: false
+  });
+
+  if (process.env.HOT) {
+    mainWindow.loadURL(`file://${__dirname}/app/hot-dev-app.html`);
+  } else {
+    mainWindow.loadURL(`file://${__dirname}/app/app.html`);
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.openDevTools();
+  }
+
+  if (process.platform === 'darwin') {
+    template = [{
+      label: 'Configurator',
+      submenu: [{
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() {
+          app.quit();
+        }
+      }]
+    }, {
+      label: 'Edit',
+      submenu: [{
+        label: 'Undo',
+        accelerator: 'Command+Z',
+        selector: 'undo:'
+      }, {
+        label: 'Redo',
+        accelerator: 'Shift+Command+Z',
+        selector: 'redo:'
+      }, {
+        type: 'separator'
+      }, {
+        label: 'Cut',
+        accelerator: 'Command+X',
+        selector: 'cut:'
+      }, {
+        label: 'Copy',
+        accelerator: 'Command+C',
+        selector: 'copy:'
+      }, {
+        label: 'Paste',
+        accelerator: 'Command+V',
+        selector: 'paste:'
+      }, {
+        label: 'Select All',
+        accelerator: 'Command+A',
+        selector: 'selectAll:'
+      }]
+    }];
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+  } else {
+    template = [{
+      label: '&File',
+      submenu: [{
+        label: '&Close',
+        accelerator: 'Ctrl+W',
+        click() {
+          mainWindow.close();
+        }
+      }]
+    }];
+    menu = Menu.buildFromTemplate(template);
+    mainWindow.setMenu(menu);
+  }
+});
